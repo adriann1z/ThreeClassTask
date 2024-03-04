9a59a01e5edeebe4975a18ab70c0af2e17ecac6e
Solomon Oduyebo <124704556+SolomonGM@users.noreply.github.com>
2024-03-04 19:37:51 +0000

    Update Testing.cs

--- Testing.cs
+++ Testing.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,23 @@ namespace Three_Class_Task
 {
     class Testing
     {
+        Game game = new Game(); //Instntiates the game class (creates an object of the game class)
+        public void TestRollDices() //Method to test the RollDices method
+        {
+            game.RollDices(); //Calls the RollDices method
+
+            int[] rolls = game.RollThree(); //Calls the RollThree method and stores the return value in the rolls array
+            int sumOfRolls = 0; //Variable to hold the sum of the rolls
+
+            for (int i = 0; i < rolls.Length; i++) //Loops through the rolls array
+            {
+                Debug.Assert(rolls[i] >= 1 && rolls[i] <= 6, $"Test Failed: {rolls[i]}. Roll should be between 1 and 6."); //Asserts that the roll of each dice object is between 1 and 6
+                sumOfRolls += rolls[i]; //Adds the roll of each dice object to the sum of the rolls
+            }
+
+            int exceptedSum = rolls.Sum(); //Calculates the sum of the rolls
+            Debug.Assert(sumOfRolls == exceptedSum, $"Test Failed: {sumOfRolls}. Sum of rolls should be {exceptedSum}."); //Asserts that the sum of the rolls is equal to the expected sum
+        }
+
     }
 }
